34e2a39ff01d2a748120b4039ee7be1f59686248
astronomersiva <astronomersiva@users.noreply.github.com>
2014-08-10 19:06:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,25 +29,35 @@
                             var weatherCode = dataWeGotViaJsonp.weather[0]['id'];
                             var weatherNow = dataWeGotViaJsonp.weather[0]['description'];                            
                             document.getElementById("weather").innerHTML = weatherNow;
-                            document.getElementById("weatherParams").innerHTML = 'Temperature: '+temp+'&degC<br>'+'Humidity: '+humidity+'&#37;<br>Max. temp: '+temp_max+'&degC<br>Min. temp: '+temp_min+'&degC';
+                            document.getElementById("weatherParams").innerHTML = 'Temperature: '+temp+'&degC<br>'+'Humidity: '
+                                        +humidity+'&#37;<br>Max. temp: '+temp_max+'&degC<br>Min. temp: '+temp_min+'&degC';
                             switch(true){
-                            case (weatherCode == 800):   document.getElementById("icon").innerHTML = '<i class="wi wi-day-sunny"></i>';
+                            case (weatherCode == 800):   document.getElementById("icon").innerHTML = 
+                                        '<i class="wi wi-day-sunny"></i>';
                             		break;
-                            case (weatherCode == 801):	document.getElementById("icon").innerHTML = '<i class="wi wi-day-cloudy"></i>';
+                            case (weatherCode == 801):	document.getElementById("icon").innerHTML = 
+                                        '<i class="wi wi-day-cloudy"></i>';
                             		break;
-                            case (weatherCode == 802):	document.getElementById("icon").innerHTML = '<i class="wi wi-day-cloudy"></i>';
+                            case (weatherCode == 802):	document.getElementById("icon").innerHTML = 
+                                        '<i class="wi wi-day-cloudy"></i>';
                             		break;
-                            case (weatherCode == 803):	document.getElementById("icon").innerHTML = '<i class="wi wi-day-cloudy"></i>';
+                            case (weatherCode == 803):	document.getElementById("icon").innerHTML = 
+                                        '<i class="wi wi-day-cloudy"></i>';
                             		break;
-                            case (weatherCode == 804):	document.getElementById("icon").innerHTML = '<i class="wi wi-cloudy"></i>';
+                            case (weatherCode == 804):	document.getElementById("icon").innerHTML = 
+                                        '<i class="wi wi-cloudy"></i>';
                             		break;
-                            case (weatherCode >= 200 && weatherCode <= 232):   document.getElementById("icon").innerHTML = '<i class="wi wi-thunderstorm"></i>';
+                            case (weatherCode >= 200 && weatherCode <= 232):   
+                                    document.getElementById("icon").innerHTML = '<i class="wi wi-thunderstorm"></i>';
                             		break;
-                            case (weatherCode >= 300 && weatherCode <= 321):   document.getElementById("icon").innerHTML = '<i class="wi wi-day-sprinkle"></i>';
+                            case (weatherCode >= 300 && weatherCode <= 321):   
+                                    document.getElementById("icon").innerHTML = '<i class="wi wi-day-sprinkle"></i>';
                             		break;
-                            case (weatherCode >= 500 && weatherCode <= 531):   document.getElementById("icon").innerHTML = '<i class="wi wi-day-rain"></i>';
+                            case (weatherCode >= 500 && weatherCode <= 531):  
+                                    document.getElementById("icon").innerHTML = '<i class="wi wi-day-rain"></i>';
                             		break;
-                            case (weatherCode >= 600 && weatherCode <= 622):   document.getElementById("icon").innerHTML = '<i class="wi wi-day-snow"></i>';
+                            case (weatherCode >= 600 && weatherCode <= 622):   
+                                    document.getElementById("icon").innerHTML = '<i class="wi wi-day-snow"></i>';
                             		break;   
                             default:	document.getElementById("icon").innerHTML = '<i class="wi wi-day-fog"></i>';                         		
                             
